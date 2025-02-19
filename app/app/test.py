46b60eb0f886372fc3a49f49be4239f5ee1714ba
Onesso <odhiambofrank1965@gmail.com>
2025-02-19 17:40:18 +0300

    linting

--- app/app/test.py
+++ app/app/test.py
@@ -1,4 +1,5 @@
-"""we'll be using the simpleTestcase since the codee does not have a database."""
+"""we'll be using the simpleTestcase since the code does not have a database."""
+
 
 from django.test import SimpleTestCase
 from app import calc
@@ -6,8 +7,9 @@ from app import calc
 
 class TestCalc(SimpleTestCase):
     """testing the calc function"""
+
     def test_add(self):
-        responce = calc.add(3,8)
+        responce = calc.add(3, 8)
         self.assertEqual(responce, 11)
 
     def test_subtract(self):
