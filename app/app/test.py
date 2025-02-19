397795af88fc71484beec6181615ad631a322993
Onesso <odhiambofrank1965@gmail.com>
2025-02-19 13:45:44 +0300

    fixing linting issues

--- app/app/test.py
+++ app/app/test.py
@@ -1,16 +1,10 @@
 """we'll be using the simpleTestcase since the codee does not have a database."""
 
 from django.test import SimpleTestCase
-
 from app import calc
 
 class TestCalc(SimpleTestCase):
-
     """testing the calc function"""
-
     def test_add(self):
-        responce  = calc.add(3,8)
+        responce = calc.add(3,8)
         self.assertEqual(responce, 11)
-        
-        
-
